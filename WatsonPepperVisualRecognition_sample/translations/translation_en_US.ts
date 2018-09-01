490c8f47a65594b6cd0065786372788bef29ced3
birdtomita <bird.tomita@gmail.com>
2018-09-01 10:04:09 +0900

    init

---
+++ WatsonPepperVisualRecognition_sample/translations/translation_en_US.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>PepperとWatsonで画像認識をしますよー。左右どちらかのバンパーで撮影開始。タブレットタッチで終了します。</source>
+            <comment>Text</comment>
+            <translation type="unfinished">PepperとWatsonで画像認識をしますよー。左右どちらかのバンパーで撮影開始。タブレットタッチで終了します。</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
